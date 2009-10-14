10049174fd3269dc70a951e9e7e8466ac9dcb8d9
brian@mozdev.org <brian@mozdev.org@4eb1ac78-321c-0410-a911-ec516a8615a5>
2009-10-14 16:04:58 +0000

    Add-on Skeleton

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-mkdir -p chrome;
 [ -f acr.xpi ] && rm acr.xpi;
 [ -f chrome/acr.jar ] && rm chrome/acr.jar;
+cd chrome
 zip -r0 acr.jar content locale skin -x \*.svn/* -x \*.zip -x \*.db -x \*.xcf -x \*.\*~ -x \*.DS_Store;
-mv acr.jar chrome/
-zip -r9 acr.xpi chrome.manifest install.rdf defaults/ chrome/ components/ -x \*.svn/* -x \*.DS_Store;
+cd ..
+zip -r9 acr.xpi chrome.manifest install.rdf defaults/ chrome/ -x \*.svn/* -x \*.DS_Store;
 printf "ACR build finished.\n";
---
+++ defaults/preferences/prefs.js
@@ -0,0 +1,6 @@
+pref("extensions.acr.boolpref", false);
+pref("extensions.acr.intpref", 0);
+pref("extensions.acr.stringpref", "A string");
+
+// See http://kb.mozillazine.org/Localize_extension_descriptions
+pref("extensions.compatibility@addons.mozilla.org.description", "chrome://acr/locale/acr.properties");
