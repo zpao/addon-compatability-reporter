26a24b60edf7500b76d7ab4999d4b6281d0572a5
dave@33eels.com <dave@33eels.com@4eb1ac78-321c-0410-a911-ec516a8615a5>
2010-10-12 17:41:21 +0000

    Bug 575264 - [ACR Extension] Clean-up files in the acr.xpi archive

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 [ -f acr.xpi ] && rm acr.xpi;
 [ -f chrome/acr.jar ] && rm chrome/acr.jar;
 cd chrome
-zip -r0 acr.jar content locale skin -x \*.svn/* -x \*.zip -x \*.db -x \*.xcf -x \*.\*~ -x \*.DS_Store;
+zip -r0 acr.jar content locale skin -x \*.svn/* -x \*.zip -x \*.db -x \*.xcf -x \*.\*~ -x \*.DS_Store -x \*.swp;
 cd ..
-zip -r9 acr.xpi chrome.manifest install.rdf defaults/ chrome/ components/ -x \*.svn/* -x \*.DS_Store;
+zip -r9 acr.xpi chrome.manifest install.rdf defaults/ chrome/acr.jar components/ -x \*.svn/* -x \*.DS_Store -x \*.swp;
 printf "ACR build finished.\n";
