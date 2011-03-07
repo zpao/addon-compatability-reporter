39cb45142bf6bbc22e323ba68246c943a4049296
brian@mozdev.org <brian@mozdev.org@4eb1ac78-321c-0410-a911-ec516a8615a5>
2011-03-07 09:59:29 +0000

    Bug 627363: Don't create an additional .jar file for chrome content in XPI

--- build.sh
+++ build.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
 [ -f acr.xpi ] && rm acr.xpi;
-[ -f chrome/acr.jar ] && rm chrome/acr.jar;
-cd chrome
-zip -r0 acr.jar content locale skin -x \*.svn/* -x \*.zip -x \*.db -x \*.xcf -x \*.\*~ -x \*.DS_Store -x \*.swp;
-cd ..
-zip -r9 acr.xpi chrome.manifest install.rdf defaults/ chrome/acr.jar components/ -x \*.svn/* -x \*.DS_Store -x \*.swp;
+zip -r9 acr.xpi chrome.manifest install.rdf defaults/ chrome/ components/ -x \*.svn/* -x \*.DS_Store -x \*.swp;
 printf "ACR build finished.\n";
